a3dcba6beb332f519c6cd54651ab9c4db3efff91
VishnuPR <vishnu@webtreeonline.com>
2025-07-10 16:58:38 +0530

    chat improved

--- src/main/java/com/peertopeer/controller/ChatController.java
+++ src/main/java/com/peertopeer/controller/ChatController.java
@@ -1,55 +1,24 @@
 package com.peertopeer.controller;
 
+
 import com.peertopeer.entity.Message;
-import com.peertopeer.service.ChatService;
-import com.peertopeer.service.UserService;
-import com.peertopeer.vo.UserLoginVO;
-import com.peertopeer.vo.UserVO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import com.peertopeer.service.ChatService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
 
-
-    @PostMapping("/api/login")
-    public ResponseEntity<Long> login(@RequestBody UserLoginVO request) {
-        return ResponseEntity.ok(10L);
-    }
-
-
     @ResponseBody
-    @GetMapping("/api/chat-history")
+    @GetMapping("/chat-history")
     public List<Message> getChatHistory(@RequestParam String conversationId) {
         return chatService.getChatHistory(conversationId);
     }
 
-
-    /// -pages- ///
-
-
-    @GetMapping("/user-list")
-    public String userList() {
-        return "user-list";
-    }
-
-    @GetMapping("/private")
-    public String chat() {
-        return "private-chat";
-    }
-
-    @GetMapping("/group")
-    public String group() {
-        return "group-chat";
-    }
 }
-
